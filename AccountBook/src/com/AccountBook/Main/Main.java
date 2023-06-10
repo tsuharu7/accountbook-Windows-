e34460bfa5147c15200fe3fb752cbf7630e5ae5d
Haruto Tsuruzono <haruto_tsuruzono@himirai.com>
2023-06-10 18:14:32 +0900

    Update Main.java

--- AccountBook/src/com/AccountBook/Main/Main.java
+++ AccountBook/src/com/AccountBook/Main/Main.java
@@ -1,5 +1,7 @@
 package com.AccountBook.Main;
 
+import com.AccountBook.Login.*;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,6 +10,10 @@ public class Main {
 		//引数:なし
 		//戻り値；String型ファイル名
 		
+		System.out.println("<<TIPS>> プログラムの入力画面でExitと入力するとプログラムを終了できます");
+		sleep(3000);
+		
+		
 		String Filename = Login.login();
 		
 		//Print File name 
@@ -15,6 +21,13 @@ public class Main {
 		System.out.println("Filename:" + Filename );
 		//testing
 
+	}
+	public static void sleep(int num) {
+		try {
+			Thread.sleep(num);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
