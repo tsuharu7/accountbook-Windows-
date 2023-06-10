bd6e83a2a8b2efaa95a73e74f06c394210c50760
Haruto Tsuruzono <haruto_tsuruzono@himirai.com>
2023-06-10 16:06:30 +0900

    作成
    Main メソッド作成
    package 作成

---
+++ AccountBook/src/com/AccountBook/Main/Main.java
@@ -0,0 +1,19 @@
+package com.AccountBook.Main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//Loginクラスloginメソッド呼び出し
+		//引数:なし
+		//戻り値；String型ファイル名
+		
+		String Filename = Login.login();
+		
+		//Print File name 
+		
+		System.out.println("Filename:" + Filename );
+
+	}
+
+}
